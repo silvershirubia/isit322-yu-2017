a0a0bb9f8f7ca6185fc936686a095213d3dfb60c
meh at school <later>
2017-04-11 17:19:29 -0700

    finish rest basics in class

--- week02-rest-basics/client/src/App.test.js
+++ week02-rest-basics/client/src/App.test.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+
+
+describe('jest test', function() {
+
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<App />, div);
+    });
+
+
 });
