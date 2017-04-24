a026663aa0f8000fdb9fcd6705aef2580e45e256
Sil at home <meh@example.com>
2017-04-24 00:57:48 -0700

    finish unit test numbers

--- week02-rest-basics/client/src/SmallNumbers.test.js
+++ week02-rest-basics/client/src/SmallNumbers.test.js
@@ -18,22 +18,81 @@ describe('jest test', function() {
     };
 
 
-    it('found', () => {
+    it('found nine before click', () => {
         const  wrapper = shallow(<SmallNumbers />);
         const nineSign = <p className="App-intro">state.nine: 0</p>
-        //const nineP = wrapper.find('p').last().debug();
-        //console.log(nineP);
+
         getFirst(wrapper);
         expect(wrapper.contains(nineSign)).toEqual(true);
     });
 
-    it('renders button click message', () => {
+    it('renders button click message 9', () => {
         const wrapper = shallow(<SmallNumbers />);
         const nineSign = <p className="App-intro">state.nine: 9</p>;
         wrapper.find('button.getNine').simulate('click');
-        const nineP = wrapper.find('p').last().debug();
-        //console.log(nineP);
         expect(wrapper.contains(nineSign)).toEqual(true);
     });
+
+    it('renders button click message 8', () => {
+        const wrapper = shallow(<SmallNumbers />);
+        const numSign = <p className="App-intro">state.eight: 8</p>;
+        wrapper.find('button.getEight').simulate('click');
+        expect(wrapper.contains(numSign)).toEqual(true);
+    });
+
+    it('renders button click message 7', () => {
+        const wrapper = shallow(<SmallNumbers />);
+        const numSign = <p className="App-intro">state.seven: 7</p>;
+        wrapper.find('button.getSeven').simulate('click');
+        expect(wrapper.contains(numSign)).toEqual(true);
+    });
+
+    it('renders button click message 6', () => {
+        const wrapper = shallow(<SmallNumbers />);
+        const numSign = <p className="App-intro">state.six: 6</p>;
+        wrapper.find('button.getSix').simulate('click');
+        expect(wrapper.contains(numSign)).toEqual(true);
+    });
+
+    it('renders button click message 5', () => {
+        const wrapper = shallow(<SmallNumbers />);
+        const numSign = <p className="App-intro">state.five: 5</p>;
+        wrapper.find('button.getFive').simulate('click');
+        expect(wrapper.contains(numSign)).toEqual(true);
+    });
+
+    it('renders button click message 4', () => {
+        const wrapper = shallow(<SmallNumbers />);
+        const numSign = <p className="App-intro">state.four: 4</p>;
+        wrapper.find('button.getFour').simulate('click');
+        expect(wrapper.contains(numSign)).toEqual(true);
+    });
+
+    it('renders button click message 3', () => {
+        const wrapper = shallow(<SmallNumbers />);
+        const numSign = <p className="App-intro">state.three: 3</p>;
+        wrapper.find('button.getThree').simulate('click');
+        expect(wrapper.contains(numSign)).toEqual(true);
+    });
+
+    it('renders button click message 2', () => {
+        const wrapper = shallow(<SmallNumbers />);
+        const numSign = <p className="App-intro">state.two: 2</p>;
+        wrapper.find('button.getTwo').simulate('click');
+        expect(wrapper.contains(numSign)).toEqual(true);
+    });
+
+    it('renders button click message 1', () => {
+        const wrapper = shallow(<SmallNumbers />);
+        const numSign = <p className="App-intro">state.one: 1</p>;
+        wrapper.find('button.getOne').simulate('click');
+        getLast(wrapper);
+        expect(wrapper.contains(numSign)).toEqual(true);
+    });
+
+
+
+
+
 
 });
--- week02-rest-basics/client/src/components/SmallNumbers.js
+++ week02-rest-basics/client/src/components/SmallNumbers.js
@@ -7,7 +7,15 @@ class SmallNumbers extends Component {
     constructor(){
         super();//always call first
         this.state = {
-            nine: '0'
+            nine: '0',
+            eight: '0',
+            seven: '0',
+            six: '0',
+            five: '0',
+            four: '0',
+            three: '0',
+            two: '0',
+            one: '0'
 
         };
     }
@@ -16,14 +24,78 @@ class SmallNumbers extends Component {
         this.setState({nine: '9'});
     };
 
+    getEight = () => {
+        this.setState({eight: '8'});
+    };
+
+    getSeven = () => {
+        this.setState({seven: '7'});
+    };
+
+    getSix = () => {
+        this.setState({six: '6'});
+    };
+
+    getFive = () => {
+        this.setState({five: '5'});
+    };
+
+    getFour = () => {
+        this.setState({four: '4'});
+    };
+
+    getThree = () => {
+        this.setState({three: '3'});
+    };
+
+    getTwo = () => {
+        this.setState({two: '2'});
+    };
+
+    getOne = () => {
+        this.setState({one: '1'});
+    };
+
     render() {
         return (
             <div className="App">
-
                 <p className="App-intro">
                     state.nine: {this.state.nine}
                 </p>
+                <p className="App-intro">
+                    state.eight: {this.state.eight}
+                </p>
+                <p className="App-intro">
+                    state.seven: {this.state.seven}
+                </p>
+                <p className="App-intro">
+                    state.six: {this.state.six}
+                </p>
+                <p className="App-intro">
+                    state.five: {this.state.five}
+                </p>
+                <p className="App-intro">
+                    state.four: {this.state.four}
+                </p>
+                <p className="App-intro">
+                    state.three: {this.state.three}
+                </p>
+                <p className="App-intro">
+                    state.two: {this.state.two}
+                </p>
+                <p className="App-intro">
+                    state.one: {this.state.one}
+                </p>
+
                 <button className='getNine' onClick={this.getNine}>Get Nine</button>
+                <button className='getEight' onClick={this.getEight}>Get Eight</button>
+                <button className='getSeven' onClick={this.getSeven}>Get Seven</button>
+                <button className='getSix' onClick={this.getSix}>Get Six</button>
+                <button className='getFive' onClick={this.getFive}>Get Five</button>
+                <button className='getFour' onClick={this.getFour}>Get Four</button>
+                <button className='getThree' onClick={this.getThree}>Get Three</button>
+                <button className='getTwo' onClick={this.getTwo}>Get Two</button>
+                <button className='getOne' onClick={this.getOne}>Get One</button>
             </div>
         );
     }
