13bcf8bce7bb1d54de1bbabc93f7f8d1a6a3ff1a
meh at school <later>
2017-05-20 13:53:43 -0700

    fixed list tests

--- GitExplorer/client/src/__tests__/GistLister.test.js
+++ GitExplorer/client/src/__tests__/GistLister.test.js
@@ -12,21 +12,34 @@ import ElfDebug from '../ElfDebug';
 const elfDebug = new ElfDebug(true, 'GistLister.test.js');
 
 describe('ElfHeader Suite', function () {
+    let gistList = {};
+
+    beforeEach(function() {
+        gistList = [{
+            url: '',
+            html_url: ''
+        }];
+
+    });
 
     it('renders the GistLister component without crashing', () => {
         const div = document.createElement('div');
-        ReactDOM.render(<GistLister />, div);
+        ReactDOM.render(<GistLister
+            gistList={gistList}
+            fetchGistList={function() {}} />, div);
     });
 
     it('renders and reads GistLister H2 text', () => {
-        const wrapper = shallow(<GistLister />);
+        const wrapper = shallow(<GistLister gistList={gistList}
+                                            fetchGistList={function() {}}/>);
         elfDebug.getFirst(wrapper, 'h2');
         const welcome = <h2>Gist Lister</h2>;
         expect(wrapper.contains(welcome)).toEqual(true);
     });
 
     it('renders GistLister UL Element', () => {
-        const wrapper = shallow(<GistLister />);
+        const wrapper = shallow(<GistLister gistList={gistList}
+                                            fetchGistList={function() {}}/>);
         elfDebug.getAll(wrapper,'ul');
         //var a = wrapper.find('ul');
         //console.log(a.length);
